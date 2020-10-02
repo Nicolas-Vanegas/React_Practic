d923ae35d7a1bd02476c43350131cfd88d35961b
Nicolás Vanegas <Nicolas.vanegas.p@hotmail.com>
2020-10-02 08:37:59 -0500

    Estructura y Estilos del Footer

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,8 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+
 import '../assets/styles/App.scss';
 
 const App = () => {
@@ -25,6 +27,8 @@ const App = () => {
           <CarouselItem />
         </Carousel>
       </Categories>
+
+      <Footer />
     </div>
   );
 };
