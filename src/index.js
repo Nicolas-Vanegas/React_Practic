a472d7d90e98cee7b8bd92b7e11334d09478f215
Nicolás Vanegas <Nicolas.vanegas.p@hotmail.com>
2020-10-02 09:34:36 -0500

    Creando la fake API para implementar esos items en NUESTRO PROYECTO

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+/* el initialState.json es una lista para hacer un API fake instalando el paquete de json Server: sudo npm install json-server -g el cual se va a encargar de hacer que este elemento sea como una API*/
